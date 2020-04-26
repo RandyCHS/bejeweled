8cc045076db43bdc7dad068cd0326ea351c7cdba
Daryl Zuniga <Daryl.Zuniga@gmail.com>
2020-04-25 18:10:06 -0700

    starting grid extension

---
+++ grid.ts
@@ -0,0 +1,12 @@
+// Add your code here
+
+//% color=#c96a3a icon="\uf279"
+//% groups='["Grid"]'
+namespace grid {
+    export class Grid {
+        
+        constructor(public width: number, public height: number) {
+            
+        }
+    }
+}
